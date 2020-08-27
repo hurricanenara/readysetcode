8585c3b19ba3ad152bcad92fa56ce77651c7dda1
naracodes <naralee0124@gmail.com>
2020-08-27 16:17:56 -0400

    solved isValid

---
+++ leetcode/valideParentheses.js
@@ -0,0 +1,26 @@
+
+const isValid = (s) => { // "([)]", "{[]}"
+    const dictionary = { "(": ")", "[": "]", "{": "}" }
+    let stack = [s[0]];
+
+    if (s === "") return true;
+
+    for (let i = 1; i < s.length; i++) {
+        let curChar = s[i];
+        let peek = stack[stack.length - 1];
+        if (curChar === dictionary[peek]) {
+            stack.pop();
+        } else {
+            stack.push(curChar);
+        }
+    }
+    return !stack.length;
+}
+
+console.log(isValid("()"));
+console.log(isValid("()[]{}"));
+console.log(isValid("(]"));
+console.log(isValid("([)]"));
+console.log(isValid("{[]}"));
+
+
