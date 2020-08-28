ff9f84e08576c2397c08305e5d57757e3311ca06
naracodes <naralee0124@gmail.com>
2020-08-28 11:19:44 -0400

    work on C++

--- C++/magig8ball.cpp
+++ C++/magig8ball.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <cstdlib>
 
-int main() {
+int main()
+{
+
     std::cout << "MAGIC 8-BALL\n";
-}
 
+    //srand sets the seed of the random number generator
+    //without srand, the answer would be the same each time
+    srand(time(NULL));
+    int answer = std::rand() % 10;
+
+    std::cout << answer;
+}
