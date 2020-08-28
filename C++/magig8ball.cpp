03629be7a7d5195cc2af1cb01096a388ad87758e
naracodes <naralee0124@gmail.com>
2020-08-28 11:24:04 -0400

    work on C++

--- C++/magig8ball.cpp
+++ C++/magig8ball.cpp
@@ -6,10 +6,49 @@ int main()
 
     std::cout << "MAGIC 8-BALL\n";
 
-    //srand sets the seed of the random number generator
-    //without srand, the answer would be the same each time
     srand(time(NULL));
     int answer = std::rand() % 10;
 
-    std::cout << answer;
+    // std::cout << answer;
+
+    if (answer == 0)
+    {
+        std::cout << "It is certain.\n";
+    }
+    else if (answer == 1)
+    {
+        std::cout << "It is decidedly so.\n";
+    }
+    else if (answer == 3)
+    {
+        std::cout << "Without a doubt.\n";
+    }
+    else if (answer == 4)
+    {
+        std::cout << "Yes - definitely.\n";
+    }
+    else if (answer == 5)
+    {
+        std::cout << "You may rely on it.\n";
+    }
+    else if (answer == 6)
+    {
+        std::cout << "Most likely.\n";
+    }
+    else if (answer == 7)
+    {
+        std::cout << "Yes.\n";
+    }
+    else if (answer == 8)
+    {
+        std::cout << "Signs point to yes.\n";
+    }
+    else if (answer == 9)
+    {
+        std::cout << "Ask again later.\n";
+    }
+    else
+    {
+        std::cout << "Very doubtful.\n";
+    }
 }
