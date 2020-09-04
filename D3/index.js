9b77e92711b2395252428b27414846cc21e9f1b6
naracodes <naralee0124@gmail.com>
2020-09-04 14:22:04 -0400

    add svg

--- D3/index.js
+++ D3/index.js
@@ -11,7 +11,20 @@ const svg = canvas.append('svg')
 
 svg.append('rect')
     .attr('width', 200)
-    .attr('height', 100);
+    .attr('height', 100)
+    .attr('fill', 'blue')
+    .attr('x', 20)
+    .attr('y', 20)
 
-svg.append('circle');
-svg.append('line');
+svg.append('circle')
+    .attr('r', 50)
+    .attr('cx', 300)
+    .attr('cy', 70)
+    .attr('fill', 'pink')
+
+svg.append('line')
+    .attr('x1', 370)
+    .attr('x2', 400)
+    .attr('y1', 20)
+    .attr('y2', 120)
+    .attr('stroke', 'red');
