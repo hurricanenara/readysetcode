160b1f86314f5a2498c5fc06ba64b04c003f789e
naracodes <naralee0124@gmail.com>
2020-11-14 23:33:08 -0500

    add fizzbuzz

--- u-code/one/exercises/maxchar/index.js
+++ u-code/one/exercises/maxchar/index.js
@@ -5,6 +5,15 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let count = {};
+    for (let char of str) {
+        count[char] = count[char] + 1 || 1;
+    }
+    let max = Math.max(...Object.values(count));
+    for (let char in count) {
+        if (count[char] === max) return char;
+    }
+}
 
 module.exports = maxChar;
