59beee515cdbe043a75003d1be07d4c43da1f467
naracodes <naralee0124@gmail.com>
2020-11-16 00:13:16 -0500

    add bs4 fundamentals

--- python/BeautifulSoup/fundamentals.py
+++ python/BeautifulSoup/fundamentals.py
@@ -8,3 +8,39 @@ webpage_response = requests.get("https://content.codecademy.com/courses/beautifu
 webpage = webpage_response.content
 
 print(webpage)
+
+# BS4 Object
+
+# import requests
+from bs4 import BeautifulSoup
+
+webpage_response = requests.get('https://content.codecademy.com/courses/beautifulsoup/shellter.html')
+
+webpage = webpage_response.content
+
+soup = BeautifulSoup(webpage, "html.parser")
+
+print(soup)
+
+# Object types
+
+# import requests
+# from bs4 import BeautifulSoup
+
+webpage_response = requests.get('https://content.codecademy.com/courses/beautifulsoup/shellter.html')
+
+webpage = webpage_response.content
+
+soup = BeautifulSoup(webpage, "html.parser")
+
+print(soup)
+
+# Navigating by Tags
+
+# import requests
+# from bs4 import BeautifulSoup
+
+webpage_response = requests.get('https://content.codecademy.com/courses/beautifulsoup/shellter.html')
+
+webpage = webpage_response.content
+soup = BeautifulSoup(webpage, "html.parser")
