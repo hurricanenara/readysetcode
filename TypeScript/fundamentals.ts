140cc27d37e460990edfeb4838a10a814a9daf73
naracodes <naralee0124@gmail.com>
2020-11-16 14:31:52 -0500

    add TS fundamentals

---
+++ TypeScript/fundamentals.ts
@@ -0,0 +1,22 @@
+// let aged = true;
+// let realAge = 0;
+
+// if (aged) {
+//   realAge = "4"; // will throw error
+// }
+
+// let dogAge = realAge * 7;
+
+// console.log(`${dogAge} years`);
+
+let aged = true;
+let realAge = 0;
+
+if (aged) {
+  realAge = 4; // reassign realAge with integer
+}
+
+let dogAge = realAge * 7;
+
+console.log(`${dogAge} years`);
+
