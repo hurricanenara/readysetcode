f58d2b98876aaa457caa29d51d9a8842fca412a5
naracodes <naralee0124@gmail.com>
2020-11-16 13:19:58 -0500

    add histogram, scatter plot, and polyfit

--- python/BeautifulSoup/cacao.py
+++ python/BeautifulSoup/cacao.py
@@ -37,9 +37,24 @@ for name in company_names[1:]:
     companies.append(name.get_text())
 # print(companies)
 
-cols = {"Company": companies, "Ratings": ratings_nums}
+cocoa_percentage = soup.select('.CocoaPercent')
+cocoa_pct_list = []
+for pct in cocoa_percentage[1:]:
+    cocoa_pct_list.append(float(pct.get_text().replace("%", "")))
+
+cols = {"Company": companies, "Ratings": ratings_nums, "Percentages": cocoa_pct_list}
 df = pd.DataFrame.from_dict(cols)
 
 avg_ratings = df.groupby('Company')['Ratings'].mean()
 top_ten = avg_ratings.nlargest(10)
 print(top_ten)
+
+plt.scatter(df.Percentages, df.Ratings)
+plt.show()
+plt.clf()
+
+z = np.polyfit(df.Percentages, df.Ratings, 1)
+line_function = np.poly1d(z)
+plt.plot(df.Percentages, line_function(df.Percentages), "r--")
+
+plt.show()
