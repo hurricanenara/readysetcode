2d22e43165a1646519e054c22265fb38f55cf785
naracodes <naralee0124@gmail.com>
2020-11-18 15:32:37 -0500

    add spiralMatrix

--- u-code/one/exercises/matrix/index.js
+++ u-code/one/exercises/matrix/index.js
@@ -16,7 +16,42 @@
 //     [10,  9,  8, 7]]
 
 function matrix(n) {
-    
+    let grid = new Array(n).fill(null).map(el => new Array(n).fill(null));
+    const moves = ["goRight", "goDown", "goLeft", "goUp"];
+    let i = 0;
+    let j = 0;
+    let num =  1;
+    let outOfRange = undefined;
+    let turns = 0;
+
+    while (typeof grid[i][j] !== 'number') {
+
+        grid[i][j] = num;
+        if ((turns % moves.length) === 0 && (grid[j + 1] !== outOfRange) && (turns % moves.length) === 0 && (grid[i][j + 1]) === null) {
+            j++;
+        } else if ((turns % moves.length) === 1 && (grid[i + 1] !== outOfRange) && (turns % moves.length) === 1 && (grid[i + 1][j]) === null) {
+            i++;
+        } else if ((turns % moves.length) === 2 && (grid[j - 1] !== outOfRange) && (turns % moves.length) === 2 && (grid[i][j - 1]) === null) {
+            j--;
+        } else if ((turns % moves.length) === 3 && (grid[i - 1] !== outOfRange) && (turns % moves.length) === 3 && (grid[i - 1][j]) === null) {
+            i--;
+        } else {
+            turns ++;
+            if (turns % moves.length === 0) {
+                j++;
+            } else if (turns % moves.length === 1) {
+                i++;
+            } else if (turns % moves.length === 2) {
+                j--;
+            } else {
+                i--;
+            }
+        }
+        num++;
+    }
+    return grid;
 }
+
+console.log(matrix(4));
 
 module.exports = matrix;
