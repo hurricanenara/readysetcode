84698bc1050a07320ed381f38a818f0e34e28c25
naracodes <naralee0124@gmail.com>
2020-11-19 22:05:44 -0500

    remove dupes

--- aA/recursion_project/lib/problems.js
+++ aA/recursion_project/lib/problems.js
@@ -116,7 +116,8 @@ function pow(base, exponent) {
 //     2-dimensional array: [['some data']]
 //     3-dimensional array: [[['some data']]]
 function flatten(data) {
-
+    if (!Array.isArray(data)) return [data];
+    return data.reduce((acc, el) => acc.concat(flatten(el)), [])
 }
 
 // Write a function, fileFinder(directories, targetFile), that accepts an object representing directories and a string respresenting a filename.
