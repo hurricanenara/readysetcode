36d283139892b9909df4b014686f694acfac09f1
naracodes <naralee0124@gmail.com>
2020-11-19 23:32:47 -0500

    add Queue

--- u-code/one/exercises/fib/index.js
+++ u-code/one/exercises/fib/index.js
@@ -19,10 +19,26 @@
 //     return fibArr[fibArr.length - 1];
 // }
 
+function memoize(fn) {
+    const cache = {};
+    return function(...args) {
+        if (cache[args]) {
+            return cache[args];
+        }
+        const result = fn.apply(this, args);
+        cache[args] = result;
+
+        return result;
+    }
+}
+
 //recursive
+//slowFib
 function fib(n) {
     if (n < 2) return n;
     return fib(n - 1) + fib(n - 2);
 }
+
+fib = memoize(fib);
 
 module.exports = fib;
