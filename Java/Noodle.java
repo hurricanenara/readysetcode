696f4792d1e69c94c8fb6b18cfeb1b238d094e85
naracodes <naralee0124@gmail.com>
2020-11-21 23:08:31 -0500

    add pho child class with super keyword

--- Java/Noodle.java
+++ Java/Noodle.java
@@ -1,8 +1,19 @@
 class Noodle {
 
     double lengthInCentimeters;
+    double widthInCentimeters;
     String shape;
+    String ingredients;
     String texture = "brittle";
+
+    Noodle(double lenInCent, double wthInCent, String shp, String ingr) {
+
+        this.lengthInCentimeters = lenInCent;
+        this.widthInCentimeters = wthInCent;
+        this.shape = shp;
+        this.ingredients = ingr;
+
+    }
 
     public void cook() {
 
@@ -11,6 +22,8 @@ class Noodle {
     }
 
     public static void main(String[] args) {
+
+
 
     }
 
