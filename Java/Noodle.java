190747bce9324ea128ad06bfe39a6e7152f97388
naracodes <naralee0124@gmail.com>
2020-11-21 23:32:52 -0500

    add dinner and biangbiang / using a child class as its parent class

--- Java/Noodle.java
+++ Java/Noodle.java
@@ -1,10 +1,10 @@
-class Noodle {
+public class Noodle {
 
-    double lengthInCentimeters;
-    double widthInCentimeters;
-    String shape;
-    String ingredients;
-    String texture = "brittle";
+    private double lengthInCentimeters;
+    private double widthInCentimeters;
+    private String shape;
+    protected String ingredients;
+    private String texture = "brittle";
 
     Noodle(double lenInCent, double wthInCent, String shp, String ingr) {
 
@@ -15,15 +15,17 @@ class Noodle {
 
     }
 
-    public void cook() {
+    final public boolean isTasty() {
 
-        this.texture = "cooked";
+        return true;
 
     }
 
     public static void main(String[] args) {
 
-
+        Ramen yasaiRamen = new Ramen();
+        System.out.println(yasaiRamen.ingredients);
+        System.out.println(yasaiRamen.isTasty());
 
     }
 
