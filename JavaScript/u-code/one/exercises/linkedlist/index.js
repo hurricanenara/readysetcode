d995229e350c736ad03048c793791c956e46ae6b
naracodes <naralee0124@gmail.com>
2020-11-22 20:15:05 -0500

    add linkedlist midpoint problem

--- JavaScript/u-code/one/exercises/linkedlist/index.js
+++ JavaScript/u-code/one/exercises/linkedlist/index.js
@@ -126,6 +126,14 @@ class LinkedList {
         }
     }
 
+    *[Symbol.iterator]() {
+        let node = this.head;
+        while (node) {
+            yield node;
+            node = node.next;
+        }
+    }
+
 }
 
 module.exports = { Node, LinkedList };
