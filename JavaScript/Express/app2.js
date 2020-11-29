63c9cefba63d6bb107c09bd780ec09b6ba48d4db
hurricanenara <naralee0124@gmail.com>
2020-11-29 18:33:13 -0500

    add next function to middleware

--- JavaScript/Express/app2.js
+++ JavaScript/Express/app2.js
@@ -3,12 +3,21 @@ const app = express();
 
 const PORT = 4001;
 
+const obj = {
+    page: 'main',
+};
+
 app.use(express.static('public'));
 
 app.use((req, res, next) => {
     console.log(`${req.method} Request Received`);
+    next();
 });
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
+
+app.get('/', (req, res, next) => {
+    res.send(obj);
+})
