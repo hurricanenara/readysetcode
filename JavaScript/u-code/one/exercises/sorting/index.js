05cdeede6d15a5407a8c0a999d59bd6f9002ec75
naracodes <naralee0124@gmail.com>
2020-12-02 12:45:38 -0500

    complete bubbleSort with checker and parallel assignment

--- JavaScript/u-code/one/exercises/sorting/index.js
+++ JavaScript/u-code/one/exercises/sorting/index.js
@@ -2,7 +2,18 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
+    let isSorted = false;
 
+    while (!isSorted) {
+        isSorted = true;
+        for (let i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+                isSorted = false;
+            }
+        }
+    }
+    return arr;
 }
 
 function selectionSort(arr) {
