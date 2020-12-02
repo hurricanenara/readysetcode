e40afb290559eeeb3599040ce993ca6f671c4893
naracodes <naralee0124@gmail.com>
2020-12-02 00:54:24 -0500

    add more explanatory way of solving validate binary search tree

--- JavaScript/u-code/one/exercises/validate/index.js
+++ JavaScript/u-code/one/exercises/validate/index.js
@@ -6,10 +6,30 @@
 // the parent
 
 function validate(node, min = null, max = null) {
-    if (min && node.data < min || max && node.data > max) return false;
-    if (node.left) return validate(node.left, min, node.data);
-    if (node.right) return validate(node.right, node.data, max);
-    return true
+    if (max !== null && node.data > max) {
+        return false;
+    }
+
+    if (min !== null && node.data < min) {
+        return false;
+    }
+
+    if (node.left && !validate(node.left, min, node.data)) {
+        return false;
+    }
+
+    if (node.right && !validate(node.right, node.data, max)) {
+        return false;
+    }
+
+    return true;
 }
+
+// function validate(node, min = null, max = null) {
+//     if (min && node.data < min || max && node.data > max) return false;
+//     if (node.left) return validate(node.left, min, node.data);
+//     if (node.right) return validate(node.right, node.data, max);
+//     return true
+// }
 
 module.exports = validate;
