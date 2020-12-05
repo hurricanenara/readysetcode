e9c116b3c1f54e629b261d44bdea55b982d5defd
naracodes <naralee0124@gmail.com>
2020-12-05 01:45:43 -0500

    add stop_words using stopwords

--- python/Natural Language Processing/script.py
+++ python/Natural Language Processing/script.py
@@ -1,4 +1,8 @@
 import re
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, sent_tokenize
+
+# Noise Removal
 
 headline_one = '<h1>Nation\'s Top Pseudoscientists Harness High-Energy Quartz Crystal Capable Of Reversing Effects Of Being Gemini</h1>'
 
@@ -8,3 +12,29 @@ headline_no_tag = re.sub(r'<.?h1>', '', headline_one)
 print(headline_no_tag)
 
 tweet_no_at = re.sub(r'@', '', tweet)
+
+# Tokenization
+
+ecg_text = 'An electrocardiogram is used to record the electrical conduction through a person\'s heart. The readings can be used to diagnose cardiac arrhythmias.'
+
+tokenized_by_word = word_tokenize(ecg_text)
+
+tokenized_by_sentence = sent_tokenize(ecg_text)
+
+# Normalization
+
+brands = 'Salvation Army, YMCA, Boys & Girls Club of America'
+
+brands_lower = brands.lower()
+
+brands_upper = brands.upper()
+
+# Stopword Removal
+
+stop_words = set(stopwords.words('english'))
+
+survey_text = 'A YouGov study found that American\'s like Italian food more than any other country\'s cuisine.'
+
+tokenized_survey = word_tokenize(survey_text)
+
+text_no_stops = [word for word in tokenized_survey if word not in stop_words]
