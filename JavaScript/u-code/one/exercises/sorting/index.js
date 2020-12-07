1dac70076bfabc2ded9e3675127bd4b7fc9c705d
naracodes <naralee0124@gmail.com>
2020-12-06 20:39:44 -0500

    complete mergeSort

--- JavaScript/u-code/one/exercises/sorting/index.js
+++ JavaScript/u-code/one/exercises/sorting/index.js
@@ -33,11 +33,24 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
-
+    if (arr.length ===1) {
+        return arr;
+    }
+    const center = Math.floor(arr.length / 2);
+    const left = arr.slice(0, center); // not including center by calling slice
+    
 }
 
 function merge(left, right) {
-
+    const results = [];
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            results.push(left.shift());
+        } else {
+            results.push(right.shift());
+        }
+    }
+    return [...results, ...left, ...right];
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
