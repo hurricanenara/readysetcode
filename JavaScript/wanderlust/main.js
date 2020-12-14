12c731f9a01d3deb6262b78e57123a49e4efe5e3
naracodes <naralee0124@gmail.com>
2020-12-14 13:04:19 -0500

    build out venue function to GET venues via foursquare

--- JavaScript/wanderlust/main.js
+++ JavaScript/wanderlust/main.js
@@ -19,7 +19,12 @@ const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Frida
 // Add AJAX functions here:
 const getVenues = async () => {
     const city = $input.val();
+    const urlToFetch = `${url}${city}&limit=10&client_id=${clientId}&client_secret=${clientSecret}&v=20201214`;
+    try {
 
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 const getForecast = () => {
