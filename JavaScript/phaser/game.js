82c572cc47cf699353dc5bcdc9b8b915a2a20f24
naracodes <naralee0124@gmail.com>
2020-12-15 20:55:12 -0500

    add sprite to game canvas

--- JavaScript/phaser/game.js
+++ JavaScript/phaser/game.js
@@ -35,4 +35,27 @@ const config = {
 //     }
 // };
 
+//add sprite
+
+// function preload() {
+//     // Load in the sprite here!
+//     this.load.image('codey', "https://content.codecademy.com/courses/learn-phaser/codey.png");
+// }
+
+// function create() {
+//     // Create a sprite game object here!
+//     this.add.sprite(40, 40, 'codey');
+// }
+
+// const config = {
+//     type: Phaser.AUTO,
+//     width: 450,
+//     height: 600,
+//     backgroundColor: "#5f2a55",
+//     scene: {
+//         create,
+//         preload
+//     }
+// }
+
 const game = new Phaser.Game(config);
