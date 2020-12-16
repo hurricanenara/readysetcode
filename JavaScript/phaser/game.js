2d9c18c4d982d8f02eed3f7ea15f35a2191d55f6
naracodes <naralee0124@gmail.com>
2020-12-15 23:13:39 -0500

    add update to make sprite move across canvas

--- JavaScript/phaser/game.js
+++ JavaScript/phaser/game.js
@@ -93,4 +93,36 @@ const config = {
 const game = new Phaser.Game(config);
 */
 
+// moving codey with update function
+
+/*
+let codey;
+
+function preload() {
+    this.load.image('codey', 'https://content.codecademy.com/courses/learn-phaser/codey.png');
+}
+
+function create() {
+    codey = this.add.sprite(30, 200, 'codey')
+}
+
+// Create your update() function here
+function update() {
+    codey.x += 1;
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 400,
+    height: 400,
+    backgroundColor: "#5f2a55",
+    scene: {
+        preload,
+        create,
+        // Include update here!
+        update,
+    }
+}
+*/
+
 const game = new Phaser.Game(config);
