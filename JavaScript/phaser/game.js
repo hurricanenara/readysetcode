23f41f264404365869c71a6a4f30770fbb12709b
naracodes <naralee0124@gmail.com>
2020-12-15 20:57:41 -0500

    add image

--- JavaScript/phaser/game.js
+++ JavaScript/phaser/game.js
@@ -58,4 +58,27 @@ const config = {
 //     }
 // }
 
+// add image
+
+// function preload() {
+//     // Load in the background image here!
+//     this.load.image('sky', 'https://content.codecademy.com/courses/learn-phaser/sky.jpg');
+// }
+
+// function create() {
+//     // Put the background image in the scene here!
+//     this.add.image(-100, 300, 'sky');
+// }
+
+// const config = {
+//     type: Phaser.AUTO,
+//     width: 450,
+//     height: 600,
+//     backgroundColor: "#5f2a55",
+//     scene: {
+//         create,
+//         preload
+//     }
+// }
+
 const game = new Phaser.Game(config);
