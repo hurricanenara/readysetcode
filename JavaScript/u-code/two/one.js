b3e5027d170537e731b5c2614349386ee299bca4
naracodes <naralee0124@gmail.com>
2020-12-26 17:05:05 -0500

    refactor function same with two objects for time complexity

--- JavaScript/u-code/two/one.js
+++ JavaScript/u-code/two/one.js
@@ -36,18 +36,43 @@ function isAlphaNumeric(char) {
 //     return !arr2.length;
 // }
 
+// function same(arr1, arr2) {
+//     if (arr1.length !== arr2.length) return false;
+//     for (let i = 0; i < arr1.length; i++) {
+//         const correctIndex = arr2.indexOf(arr1[i] ** 2);
+//         if (correctIndex === -1) {
+//             return false;
+//         }
+//         arr2.splice(correctIndex, 1);
+//     }
+//     return true;
+// }
+
+// refactored
+
 function same(arr1, arr2) {
     if (arr1.length !== arr2.length) return false;
-    for (let i = 0; i < arr1.length; i++) {
-        const correctIndex = arr2.indexOf(arr1[i] ** 2);
-        if (correctIndex === -1) {
+
+    const freq1 = {};
+    const freq2 = {};
+
+    for (let el of arr1) {
+        freq1[el] = ++freq1[el] || 1;
+    }
+
+    for (let el of arr2) {
+        freq2[el] = ++freq2[el] || 1;
+    }
+
+    for (let key in freq1) {
+        const squaredNum = key ** 2;
+        if (!((squaredNum in freq2) && (freq1[key] === freq2[squaredNum]))) {
             return false;
         }
-        arr2.splice(correctIndex, 1);
     }
     return true;
 }
 
-console.log(same([1, 2, 3], [4, 1, 9]));
-console.log(same([1, 2, 3], [1, 9]));
-console.log(same([1, 2, 1], [4, 4, 1]));
+console.log(same([1, 2, 3], [4, 1, 9])); //true
+// console.log(same([1, 2, 3], [1, 9]));
+console.log(same([1, 2, 1], [4, 4, 1])); //false
