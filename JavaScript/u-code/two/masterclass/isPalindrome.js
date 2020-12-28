b9d1e7f8a02170915d4b2b05c44d2b5ad10eb6ad
naracodes <naralee0124@gmail.com>
2020-12-28 12:07:51 -0500

    add another solution to isPalindrome

--- JavaScript/u-code/two/masterclass/isPalindrome.js
+++ JavaScript/u-code/two/masterclass/isPalindrome.js
@@ -7,6 +7,14 @@ function isPalindrome(str){
     return str === reversed(str);
 }
 
+// other solution
+// function isPalindrome(str){
+//     if(str.length === 1) return true;
+//     if(str.length === 2) return str[0] === str[1];
+//     if(str[0] === str.slice(-1)) return isPalindrome(str.slice(1,-1))
+//     return false;
+// }
+
 console.log(isPalindrome('awesome')) // false
 console.log(isPalindrome('foobar')) // false
 console.log(isPalindrome('tacocat')) // true
