c8f045e86fa912bebfe3dc7c3c12cf9755c9cabe
naracodes <naralee0124@gmail.com>
2020-12-27 21:46:05 -0500

    add more great solutions to nth fib

--- JavaScript/u-code/two/masterclass/nthFib.js
+++ JavaScript/u-code/two/masterclass/nthFib.js
@@ -14,6 +14,33 @@ function fibHelper(num) {
     return fibArr;
 }
 
+// Other great solutions
+
+// 1. O(N) T | O(N) S
+
+function fib(num, memoize = {1: 0, 2: 1}) {
+  if (n in memoize) {
+    return memoize[n];
+  } else {
+    memoize[n] = fib(num - 1, memoize) + fib(num - 2, memoize);
+    return memoize[n];
+  }
+}
+
+// 2. O(N) T | O(1) S
+// non-recursive
+function fib(num) {
+  const lastTwo = [0, 1];
+  let counter = 3;
+  while (counter <= n) {
+    const nextFib = lastTwo[0] + lastTwo[1];
+    lastTwo[0] = lastTwo[1];
+    lastTwo[1] = nextFib;
+    counter++;
+  }
+  return num > 1 ? lastTwo[1] : lastTwo[0];
+}
+
 console.log(fibHelper(4));
 console.log(fib(4));
 console.log(fib(10));
