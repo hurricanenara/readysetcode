1418c6da608b371078c5d44e573a059d5bcec64a
naracodes <naralee0124@gmail.com>
2020-12-29 13:45:22 -0500

    add stringSearch

---
+++ JavaScript/u-code/two/masterclass/searchingAlgos/stringSearch.js
@@ -0,0 +1,13 @@
+
+
+function stringSearch(str, target) {
+    let count = 0;
+    for (let i = 0; i <= (str.length - target.length); i++) {
+        if (str.slice(i, i + target.length) === target) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(stringSearch("hellolo", "lo"));
