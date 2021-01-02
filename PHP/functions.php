ad9b98f18202b08f1c8b2e390787e60a5708d637
naracodes <naralee0124@gmail.com>
2021-01-01 23:50:13 -0500

    add more functions

--- PHP/functions.php
+++ PHP/functions.php
@@ -7,3 +7,19 @@ function sayHello()
 }
 
 sayHello();
+
+function increaseEnthusiasm($str)
+{
+  return $str . "!";
+}
+
+echo increaseEnthusiasm("Google");
+
+function repeatThreeTimes($str)
+{
+  return $str . $str . $str;
+}
+
+echo repeatThreeTimes("Google");
+echo "\n";
+echo increaseEnthusiasm(repeatThreeTimes("Google"));
