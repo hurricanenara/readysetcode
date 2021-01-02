e43c144d072fd3b1aaf7ad75ed80e022e614d9b9
naracodes <naralee0124@gmail.com>
2021-01-02 13:20:15 -0500

    add built-in functions for strings

--- PHP/functions.php
+++ PHP/functions.php
@@ -6,20 +6,47 @@ function sayHello()
     echo "\n";
 }
 
-sayHello();
-
+// sayHello();
 function increaseEnthusiasm($str)
 {
   return $str . "!";
 }
 
-echo increaseEnthusiasm("Google");
-
+// echo increaseEnthusiasm("Google");
 function repeatThreeTimes($str)
 {
   return $str . $str . $str;
 }
 
-echo repeatThreeTimes("Google");
-echo "\n";
-echo increaseEnthusiasm(repeatThreeTimes("Google"));
+// echo repeatThreeTimes("Google");
+// echo "\n";
+// echo increaseEnthusiasm(repeatThreeTimes("Google"));
+
+// built-in functions
+
+$name = "nara";
+$age = 100;
+$paragraph = "Nara dusty rose matcha coffee spring summer fall Nara winter";
+
+// echo gettype($name);
+// echo "\n";
+// echo gettype($age);
+// echo "\n";
+// var_dump($name);
+// var_dump($age);
+
+// String Functions
+
+// echo strrev("Nara");
+// echo "\n";
+// echo strtolower("Nara");
+// echo "\n";
+// echo strtoupper("Nara");
+// echo "\n";
+// echo str_repeat("Nara", 5);
+// echo "\n";
+
+
+// Substring
+
+echo substr_count($paragraph, "Nara");
