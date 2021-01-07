ed0711849dc0b83019a25f384f058ac40d0bb1b2
naracodes <naralee0124@gmail.com>
2021-01-07 16:52:45 -0500

    add radixSort

--- JavaScript/u-code/two/masterclass/sortingAlgos/integer/radixSort.js
+++ JavaScript/u-code/two/masterclass/sortingAlgos/integer/radixSort.js
@@ -1,10 +1,13 @@
-const { get } = require("http");
-
 
 function getDigit(digit, index) {
     const base = 10;
     const power = index + 1;
     return Math.floor((Math.abs(digit) % base**(power)) / (base ** (power - 1)));
+}
+
+// stackoverflow function
+function getDigitShort(digit, index) {
+    return Math.floor(Math.abs(num) / Math.pow(10, index)) % 10;
 }
 
 // console.log(getDigit(-12345, 0));
@@ -50,4 +53,18 @@ function mostDigits(digitArr) {
     // return max;
 }
 
-console.log(mostDigits([1, 2, 34, 676, 12, 12345]));
+// console.log(mostDigits([1, 2, 34, 676, 12, 12345]));
+
+function radixSort(nums) {
+    for (let k = 0; k < mostDigits(nums); k++) {
+        let digitBuckets = Array.from({length: 10}, () => []);
+        for (let i = 0; i < nums.length; i++) {
+            const bucketNum = getDigit(nums[i], k);
+            digitBuckets[bucketNum].push(nums[i]);
+        }
+        nums = [].concat(...digitBuckets); // basically flattens nested arrays
+    }
+    return nums;
+}
+
+console.log(radixSort([23, 345, 5467, 12, 2345, 9852]));
