807a6b341f93d8bfcfec444b0424ac9b748fde50
naracodes <naralee0124@gmail.com>
2021-01-07 23:08:30 -0500

    complete mergeRanges

---
+++ JavaScript/u-code/three/mergeRanges.js
@@ -0,0 +1,52 @@
+
+function mergeRanges(meetings) {
+    const sortedMeetings = meetings.sort((a, b) => {
+        return a.startTime - b.startTime;
+    });
+
+    const mergedMeetings = [];
+
+    for (let i = 0; i < meetings.length - 1; i++) {
+        console.log(i, "entered into for loop")
+        const leftMeeting = meetings[i];
+        const rightMeeting = meetings[i + 1];
+
+        if (leftMeeting.endTime >= rightMeeting.startTime) {
+            const mergedMeeting = { startTime: leftMeeting.startTime, endTime: Math.max(leftMeeting.endTime, rightMeeting.endTime) };
+            mergedMeetings.push(mergedMeeting);
+            i++;
+        } else {
+            mergedMeetings.push(leftMeeting);
+        }
+    }
+    return mergedMeetings;
+}
+
+//first attempt! Don't be afraid to try out your thoughts
+
+// function mergeRanges(meetings) {
+//     let mergedMeetings = [];
+
+//     for (let i = 0; i < meetings.length; i++) {
+//         const meetingOne = meetings[i];
+//         for (let j = 0; j < meetings.length; j++) {
+//             const meetingTwo = meetings[j];
+//             if (i !== j) {
+//                 if (meetingOne.endTime >= meetingTwo.startTime) {
+//                     const mergedMeeting = { startTime: meetingOne.startTime, endTime: Math.max(meetingOne.endTime, meetingTwo.endTime) };
+//                     mergedMeetings.push(mergedMeeting);
+//                 }
+//             }
+//         }
+//     }
+//     return mergedMeetings;
+// }
+
+// console.log(mergeRanges([{ startTime: 1, endTime: 3 }, { startTime: 2, endTime: 4 }]));
+console.log(mergeRanges(  [
+  { startTime: 0,  endTime: 1 },
+  { startTime: 3,  endTime: 5 },
+  { startTime: 4,  endTime: 8 },
+  { startTime: 10, endTime: 12 },
+  { startTime: 9,  endTime: 10 },
+]));
