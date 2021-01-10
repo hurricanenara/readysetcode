6e0540ec20d2f082b2220a76cab7ee03d5486e35
naracodes <naralee0124@gmail.com>
2021-01-09 20:43:19 -0500

    add push method dll

--- JavaScript/u-code/two/masterclass/ds/dll.js
+++ JavaScript/u-code/two/masterclass/ds/dll.js
@@ -14,5 +14,35 @@ class DoublyLinkedList {
         this.length = 0;
     }
 
-    
+    push(val) {
+        const newNode = new Node(val);
+        if (!this.length) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++
+    }
+
+    forEach(cb) {
+        let currentNode = this.head;
+        while (currentNode) {
+            // cb.call(this, currentNode);
+            cb(currentNode);
+            currentNode = currentNode.next;
+        }
+    }
 }
+
+const list = new DoublyLinkedList();
+list.push("A");
+list.push("B");
+// list.push("C");
+// list.push("D");
+// console.log(list.forEach(node => {
+//     console.log(`${node.val}, head: ${list.head.val === node.val}, tail: ${list.tail.val === node.val}`);
+// }));
+console.log(list);
