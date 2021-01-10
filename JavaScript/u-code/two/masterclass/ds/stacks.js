9e6d1e8f5dae918f2e811f74bab07cab0d16cf8f
naracodes <naralee0124@gmail.com>
2021-01-10 11:31:43 -0500

    implement stack ds with singly linked list

---
+++ JavaScript/u-code/two/masterclass/ds/stacks.js
@@ -0,0 +1,71 @@
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    push(val) {
+        const newNode = new Node(val);
+        if (!this.size) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            const lastNode = this.first;
+            this.first = newNode;
+            newNode.next = lastNode;
+        }
+        return ++this.size;
+    }
+
+    pop() {
+        if (!this.size) return null;
+
+        const topNode = this.first;
+        
+        if (this.size === 1) {
+            this.first = null;
+            this.last = null;
+        } else {
+            const nextNode = topNode.next;
+            this.first = nextNode;
+            topNode.next = null;
+        }
+        this.size--;
+        return topNode.val;
+    }
+
+
+    forEach(cb) {
+        let currentNode = this.first;
+        while (currentNode) {
+            // cb.call(this, currentNode);
+            cb(currentNode);
+            currentNode = currentNode.next;
+        }
+    }
+}
+
+const stack = new Stack();
+stack.push("A");
+stack.push("B");
+stack.push("C");
+stack.push("D");
+stack.pop();
+// stack.pop();
+// stack.pop();
+
+console.log(stack);
+
+console.log(stack.forEach(node => {
+    console.log(`${node.val}, first: ${stack.first.val === node.val}, last: ${stack.last.val === node.val}
+    size: ${stack.size}`);
+}));
