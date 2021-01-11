f3df410eb184b4c2cf79965309dda7c0c2826cbf
naracodes <naralee0124@gmail.com>
2021-01-11 11:59:35 -0500

    start dfs preorder method

--- JavaScript/u-code/two/masterclass/ds/treeTraversal.js
+++ JavaScript/u-code/two/masterclass/ds/treeTraversal.js
@@ -11,6 +11,7 @@ class BinaryTree {
     constructor() {
         this.root = null;
         this.queue = [];
+        this.stack = [];
         this.visited = [];
     }
 
@@ -19,22 +20,41 @@ class BinaryTree {
         q.push(this.root);
         let currentNode = q[0];
         while (currentNode) {
-            debugger
-            if (currentNode.left) {
-                debugger
-                q.push(currentNode.left);
-            }
-            if (currentNode.right) {
-                debugger
-                q.push(currentNode.right);
-            }
-            debugger
-            this.visited.push(q.shift());
+            if (currentNode.left) q.push(currentNode.left);
+            if (currentNode.right) q.push(currentNode.right);
+            this.visited.push(q.shift().val);
             currentNode = q[0];
         }
-        debugger
         return this.visited;
     }
+    
+    dfsIterative() {
+        
+    }
+
+    // dfsPreOrder() {
+    //     debugger
+    //     const visited = [];
+    //     let currentNode = this.root;
+
+    //     function dfsTraverse(node) {
+    //         debugger
+    //         visited.push(node.val);
+    //         if (node.left) {
+    //             debugger
+    //             dfsTraverse(node.left);
+    //         }
+    //         if (node.right) {
+    //             debugger
+    //             dfsTraverse(node.right);
+    //         }
+    //     }
+    //     debugger
+    //     dfsTraverse(currentNode);
+    //     debugger
+    //     return visited;
+    // }
+    
 }
 
 const tree = new BinaryTree();
@@ -51,4 +71,6 @@ root.right.left = new Node(11);
 root.right.right = new Node(16);
 
 // console.log(root);
-console.log(tree.bfs());
+// console.log(tree.bfs());
+// console.log(tree.dfsPreOrder());
+// console.log(tree.dfs());
