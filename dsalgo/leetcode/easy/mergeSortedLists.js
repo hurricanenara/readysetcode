72151c15a198ee6c55ea10485176b49cdd5e5720
naracodes <naralee0124@gmail.com>
2021-01-16 10:20:03 -0500

    solve mergeTwoLists

---
+++ dsalgo/leetcode/easy/mergeSortedLists.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+    
+    let head = new ListNode(0);
+    let pointer = head;
+    
+    while (true) {
+        if (l1 === null && l2 === null) {
+            return null;
+        } else if (l1 === null) {
+            pointer.next = l2;
+            break;
+        } else if (l2 === null) {
+            pointer.next = l1;
+            break;
+        } else {
+            let smallerVal = 0;
+            if (l1.val < l2.val) {
+                smallerVal = l1.val;
+                l1 = l1.next;
+            } else {
+                smallerVal = l2.val;
+                l2 = l2.next;
+            }
+            const newNode = new ListNode(smallerVal);
+            pointer.next = newNode;
+            pointer = pointer.next;
+        }
+    }
+    return head.next;
+};
