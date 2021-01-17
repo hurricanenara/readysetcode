0ce89cb3b67a735ea329ce3db81345d652c9b943
naracodes <naralee0124@gmail.com>
2021-01-17 18:36:47 -0500

    solve highFive

---
+++ dsalgo/leetcode/easy/1086.highFive.js
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/high-five/
+// Runtime: 104 ms, faster than 23.02% of JavaScript online submissions for High Five.
+// Memory Usage: 41 MB, less than 62.95% of JavaScript online submissions for High Five.
+
+var highFive = function(items) {
+    const dict = {};
+    const result = [];
+    for (let i = 0; i < items.length; i++) {
+        const id = items[i][0];
+        const score = items[i][1];
+        if (dict[id]) {
+            dict[id].push(score);
+        } else {
+            dict[id] = [];
+            dict[id].push(score);
+        }
+    }
+    
+    for (let id in dict) {
+        result.push([id, getTopFiveAvg(dict[id])]);
+    }
+    console.log(dict)
+};
+
+var getTopFiveAvg = function(scores) {
+    if (scores.length === 5) {
+        return Math.floor((scores.reduce((acc, el) => acc + el)) / 5);
+    }
+    let sum = 0;
+    scores.sort((a, b) => b - a);
+    
+    for (let i = 0; i < 5; i++) {
+        sum += scores[i];
+    }
+    return Math.floor(sum / 5);
+    
+}
+
+console.log(highFive([[1,91],[1,92],[2,93],[2,97],[1,60],[2,77],[1,65],[1,87],[1,100],[2,100],[2,76]]));
--- dsalgo/leetcode/easy/122.maxProfilt2.js
+++ dsalgo/leetcode/easy/122.maxProfilt2.js
@@ -1,45 +1,55 @@
 // Runtime: 88 ms, faster than 33.39% of JavaScript online submissions for Best Time to Buy and Sell Stock II.
 // Memory Usage: 39.5 MB, less than 24.13% of JavaScript online submissions for Best Time to Buy and Sell Stock II.
 
-var maxProfit = function(prices) {
-    let i = 0;
-    let buy, sell, profit = 0;
-    let n = prices.length - 1;
-    while (i < n) {
-      while (prices[i + 1] <= prices[i]) {
-        i++;
-      }
-      buy = prices[i];
-      while (prices[i + 1] > prices[i]) {
-        i++;
-      }
-      sell = prices[i];
-      profit += (sell - buy);
-    }
-    return profit;
-};
+// var maxProfit = function(prices) {
+//     let i = 0;
+//     let buy, sell, profit = 0;
+//     let n = prices.length - 1;
+//     while (i < n) {
+//       while (prices[i + 1] <= prices[i]) {
+//         i++;
+//       }
+//       buy = prices[i];
+//       while (prices[i + 1] > prices[i]) {
+//         i++;
+//       }
+//       sell = prices[i];
+//       profit += (sell - buy);
+//     }
+//     return profit;
+// };
 
 // can this function be refactored to return all strictly increasing subarrays? like [1, 5] and [3, 6] for first example
 
+// var maxProfit = function(prices) {
+//     let i = 0;
+//     let profit = [];
+//     let buy = [];
+//     let sell = [];
+//     let n = prices.length - 1;
+//     while (i < n) {
+//       while (prices[i + 1] <= prices[i]) {
+//         i++;
+//       }
+//       buy = prices[i];
+//       while (prices[i + 1] > prices[i]) {
+//           console.log(profit, "profit")
+//         i++;
+//       }
+//       sell = prices[i];
+//       sell - buy > 0 ? profit.push([buy, sell]) : "";
+//     }
+//     return profit;
+// };
 var maxProfit = function(prices) {
-    let i = 0;
-    let profit = [];
-    let buy = [];
-    let sell = [];
-    let n = prices.length - 1;
-    while (i < n) {
-      while (prices[i + 1] <= prices[i]) {
-        i++;
-      }
-      buy = prices[i];
-      while (prices[i + 1] > prices[i]) {
-          console.log(profit, "profit")
-        i++;
-      }
-      sell = prices[i];
-      sell - buy > 0 ? profit.push([buy, sell]) : "";
+    if (prices.length <= 1) return 0;
+    let maxProfit = 0;
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] - prices[i - 1]) {
+            maxProfit += prices[i] - prices[i - 1];
+        }
     }
-    return profit;
+    return maxProfit;
 };
 
 // X
