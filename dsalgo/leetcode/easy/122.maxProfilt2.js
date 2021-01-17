2335bf913f4f47a9d598ef7416a3d106ae43a849
naracodes <naralee0124@gmail.com>
2021-01-17 13:23:01 -0500

    solve maxProfit2

---
+++ dsalgo/leetcode/easy/122.maxProfilt2.js
@@ -0,0 +1,50 @@
+// Runtime: 88 ms, faster than 33.39% of JavaScript online submissions for Best Time to Buy and Sell Stock II.
+// Memory Usage: 39.5 MB, less than 24.13% of JavaScript online submissions for Best Time to Buy and Sell Stock II.
+
+var maxProfit = function(prices) {
+    let i = 0;
+    let buy, sell, profit = 0;
+    let n = prices.length - 1;
+    while (i < n) {
+      while (prices[i + 1] <= prices[i]) {
+        i++;
+      }
+      buy = prices[i];
+      while (prices[i + 1] > prices[i]) {
+        i++;
+      }
+      sell = prices[i];
+      profit += (sell - buy);
+    }
+    return profit;
+};
+
+// can this function be refactored to return all strictly increasing subarrays? like [1, 5] and [3, 6] for first example
+
+var maxProfit = function(prices) {
+    let i = 0;
+    let profit = [];
+    let buy = [];
+    let sell = [];
+    let n = prices.length - 1;
+    while (i < n) {
+      while (prices[i + 1] <= prices[i]) {
+        i++;
+      }
+      buy = prices[i];
+      while (prices[i + 1] > prices[i]) {
+          console.log(profit, "profit")
+        i++;
+      }
+      sell = prices[i];
+      sell - buy > 0 ? profit.push([buy, sell]) : "";
+    }
+    return profit;
+};
+
+// X
+
+console.log(maxProfit([7,1,5,3,6,4]));
+console.log(maxProfit([1,2,3,4,5]));
+console.log(maxProfit([7,6,4,3,1]));
+console.log(maxProfit([3, 2, 5, 8, 1, 9]));
