d69dc813e85ca8ef7150c1d1f40e2cc5e9dbe12b
naracodes <naralee0124@gmail.com>
2021-01-18 21:02:22 -0500

    solve intersection2

---
+++ dsalgo/leetcode/easy/349.intersection.js
@@ -0,0 +1,28 @@
+
+// using Map
+// var intersection = function(nums1, nums2) {
+//   let map = new Map();
+//   for (let num of nums1) {
+//     if (!map.has(num)) {
+//         map.set(num, 1);
+//     }
+//   }
+//   return nums2.filter(n => {
+//       if (map.has(n)) {
+//         map.delete(n);
+//         return true;
+//     } else {
+//         return false;
+//     }
+//   });
+// };
+
+// using set
+function intersection(nums1 , nums2){
+    let setNum1 = new Set(nums1);
+
+    return [...new Set(nums2.filter(num => setNum1.has(num)))];
+}
+
+// console.log(intersection([1,2,2,1], [2,2]));
+console.log(intersection([4,9,5], [9,4,9,8,4]));
---
+++ dsalgo/leetcode/easy/350.intersection2.js
@@ -0,0 +1,19 @@
+var intersect = function(nums1, nums2) {
+    const map = new Map();
+    const result = [];
+
+    for (let num of nums1) {
+        map.set(num, map.get(num) + 1 || 1);
+    }
+
+    for (let num of nums2) {
+        if (map.get(num) >= 1) {
+            result.push(num);
+            map.set(num, map.get(num) - 1);
+        }
+    }
+    return result;
+}
+
+// console.log(intersect([1,2,2,1], [2,2]));
+console.log(intersect([9,4,9,8,4,5,4,9], [4,9,5]));
