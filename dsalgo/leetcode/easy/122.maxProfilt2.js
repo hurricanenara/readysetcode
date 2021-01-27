fa30b5dcd49402f2bcd91462645cf2f4ccea400f
naracodes <naralee0124@gmail.com>
2021-01-27 10:52:55 -0500

    fix error in 122.maxProfit

--- dsalgo/leetcode/easy/122.maxProfilt2.js
+++ dsalgo/leetcode/easy/122.maxProfilt2.js
@@ -45,12 +45,27 @@ var maxProfit = function(prices) {
     if (prices.length <= 1) return 0;
     let maxProfit = 0;
     for (let i = 0; i < prices.length; i++) {
-        if (prices[i] - prices[i - 1]) {
+        if (prices[i] > prices[i - 1]) {
             maxProfit += prices[i] - prices[i - 1];
         }
     }
     return maxProfit;
 };
+
+// var maxProfit = function (prices) {
+//     let profit = 0;
+
+//     for (let i = 1; i < prices.length; i++) {
+//         let prev = prices[i - 1];
+//         let current = prices[i];
+
+//         if (prev < current) {
+//             profit += current - prev;
+//         }
+//     }
+    
+//     return profit;
+// };
 
 // X
 
