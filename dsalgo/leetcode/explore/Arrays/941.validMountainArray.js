e8f7609015c9afe776417696f8119589feab4255
naracodes <naralee0124@gmail.com>
2021-02-05 11:12:25 -0500

    solve validMountainArray

---
+++ dsalgo/leetcode/explore/Arrays/941.validMountainArray.js
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/valid-mountain-array//
+
+// Runtime: 76 ms, faster than 95.20% of JavaScript online submissions for Valid Mountain Array.
+// Memory Usage: 41.6 MB, less than 57.37% of JavaScript online submissions for Valid Mountain Array.
+
+var validMountainArray = function(arr) {
+    if (arr.length < 3) return false;
+    
+    let isDecreasing = false;
+    let isIncreasing = false;
+    
+    for (let i = 1, j = 0; i < arr.length; i++, j++) {
+        if (arr[j] < arr[i] && !isDecreasing) {
+            isIncreasing = true;
+            continue;
+        }
+        isDecreasing = true;
+        
+        if (arr[j] <= arr[i]) return false;
+    }
+    return isDecreasing && isIncreasing;
+};
+
+console.log(validMountainArray([0,2,3,4,5,2,1,0]));
