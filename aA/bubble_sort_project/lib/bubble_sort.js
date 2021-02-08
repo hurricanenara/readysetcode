371ca47ff55374723f777748aeeb6f5b6e614bdb
naracodes <naralee0124@gmail.com>
2021-02-08 15:01:50 -0500

    solve bubbleSort and swap

--- aA/bubble_sort_project/lib/bubble_sort.js
+++ aA/bubble_sort_project/lib/bubble_sort.js
@@ -1,9 +1,22 @@
 function swap(array, idx1, idx2) {
-
+    let temp = array[idx1];
+    array[idx1] = array[idx2];
+    array[idx2] = temp;
+    return array;
 }
 
 function bubbleSort(array) {
-
+    let isSorted = false;
+    while (!isSorted) {
+        isSorted = true;
+        for (let i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                isSorted = false;
+                swap(array, i, i + 1);
+            }
+        }
+    }
+    return array;
 }
 
 
--- scheduler/quickstart.rb
+++ scheduler/quickstart.rb
@@ -79,3 +79,20 @@ scheduler.join
         })
     end
   end
+
+  require 'time'
+require 'rufus-scheduler'
+scheduler = Rufus::Scheduler.new
+
+desc "Record cash balance"
+task :record => :environment do
+    User.all.each do |user|
+      PortfoDatum.create!({
+          user_id: user.id,
+          date: Time.now - (60*60*5),
+          holdings_snapshot: user.holdings,
+          label: (Time.now - (60*60*5)).strftime("%I:%M %p"),
+          cash_balance: user.cash_balance
+        })
+    end    
+end
