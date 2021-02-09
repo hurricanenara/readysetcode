4bb678ecfd6ecac26a8426ac51fb657dba5275de
naracodes <naralee0124@gmail.com>
2021-02-09 17:00:28 -0500

    add another solution that's shorter

--- dsalgo/leetcode/explore/LinkedList/addTwoNumbers.js
+++ dsalgo/leetcode/explore/LinkedList/addTwoNumbers.js
@@ -36,6 +36,40 @@ var addTwoNumbers = function(l1, l2) {
     return head.next;
 };
 
+// shorter code
+const addTwoNumbers = function(l1, l2) {
+    let List = new ListNode();
+    let head = List;
+    let sum = 0;
+    let carry = 0;
+    
+
+    while(l1!==null||l2!==null||sum>0){
+
+        if(l1!==null){
+            sum = sum + l1.val;
+            l1 = l1.next;
+        }
+        if(l2!==null){
+            sum = sum + l2.val;
+            l2 = l2.next;
+        }
+        if(sum>=10){
+            carry = 1;
+            sum = sum - 10;
+        }
+
+        head.next = new ListNode(sum);
+        head = head.next;
+
+        sum = carry;
+        carry = 0;
+
+    }
+
+    return List.next;
+};
+
 //l1 nodes
 const node1 = new Node(9);
 const node2 = new Node(9);
