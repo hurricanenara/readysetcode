d783d942816c097105c91f46bc0a6cad3642af53
naracodes <naralee0124@gmail.com>
2021-02-15 11:27:34 -0500

    solve connect

---
+++ dsalgo/leetcode/explore/Trees/orders/preorder/connect.js
@@ -0,0 +1,33 @@
+
+// Runtime: 104 ms, faster than 59.31% of JavaScript online submissions for Populating Next Right Pointers in Each Node.
+// Memory Usage: 45.9 MB, less than 6.62% of JavaScript online submissions for Populating Next Right Pointers in Each Node.
+//iterative with BFS/Queue
+var connect = function(root) {
+    if (!root) return root;
+    let queue = [root];
+    while (queue.length) {
+        let next = [];
+        while (queue.length) {
+            let node = queue.shift();
+            node.next = queue[0] || null;
+            if (node.left) {
+                next.push(node.left);
+                next.push(node.right);
+            }            
+        }
+        queue = next;
+    }
+    return root;
+};
+
+// Runtime: 104 ms, faster than 59.31% of JavaScript online submissions for Populating Next Right Pointers in Each Node.
+// Memory Usage: 45.6 MB, less than 29.29% of JavaScript online submissions for Populating Next Right Pointers in Each Node.
+//recursion
+var connect = function(root) {
+    if (!root || !root.left) return root;
+    root.left.next = root.right;
+    root.right.next = root.next ? root.next.left : null;
+    connect(root.left);
+    connect(root.right);
+    return root;
+};
