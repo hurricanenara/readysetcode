0a913093d0840a4a37af4b7373e12d5394cae4c2
naracodes <naralee0124@gmail.com>
2021-02-17 21:40:05 -0500

    add other solution to pivot index

--- dsalgo/leetcode/explore/Arrays/pivotIndex.js
+++ dsalgo/leetcode/explore/Arrays/pivotIndex.js
@@ -1,14 +1,11 @@
 
 // solved!
 var pivotIndex = function(nums) {
-    if (!nums.length) return -1;
     let right = nums.reduce((acc, el) => acc + el);
     let left = 0;
     for (let i = 0; i < nums.length; i++) {
         right -= nums[i];
-        if (left === right) {
-            return i;
-        }
+        if (left === right) return i;
         left += nums[i];
     }
     return -1;
