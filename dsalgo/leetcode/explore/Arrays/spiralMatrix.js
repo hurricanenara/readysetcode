b6c3af3f7554be0b78b5b504ff8f35031b47b4df
naracodes <naralee0124@gmail.com>
2021-02-19 17:19:10 -0500

    add another solution to spiralMatrix

--- dsalgo/leetcode/explore/Arrays/spiralMatrix.js
+++ dsalgo/leetcode/explore/Arrays/spiralMatrix.js
@@ -18,6 +18,42 @@ var spiralOrder = function(matrix) {
     return spiraled;
 };
 
+// another way to solve
+var spiralOrder = function(matrix) {
+    const result = []; // Declare constant variable result initialzie to an empty array.
+    
+    let startRow = 0, 
+        endRow = matrix.length - 1; 
+    let startCol = 0, 
+        endCol = matrix[0].length - 1; 
+    
+    while (startCol <= endCol && startRow <= endRow) {
+        // top row 
+        for (let col = startCol; col <= endCol; col++) {
+            result.push(matrix[startRow][col]); 
+        }
+        // right row minus top column
+        for (let row = startRow + 1; row <= endRow; row++) {
+            result.push(matrix[row][endCol]); 
+        }
+        // bottom row minus last column
+        for (let col = endCol - 1; col >= startCol; col--) {
+            if (startRow === endRow) break; 
+            result.push(matrix[endRow][col]); 
+        }
+        // remaining cols
+        for (let row = endRow - 1; row > startRow; row--) {
+            if (startCol === endCol) break; 
+            result.push(matrix[row][startCol]);
+        }
+        startRow++; 
+        endRow--;
+        startCol++; 
+        endCol--;
+    }
+    return result;// Return result
+};
+
 
 // X but understood fully
 console.log(spiralOrder([[1,2,3],[4,5,6],[7,8,9]]));
