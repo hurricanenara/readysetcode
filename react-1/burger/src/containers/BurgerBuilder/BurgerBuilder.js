ea63d4e07884c18e78706b31a2209c66bc8f55ad
naracodes <naralee0124@gmail.com>
2021-03-01 13:25:08 -0500

    complete modal, and add cancel function on modal click

--- react-1/burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ react-1/burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,6 +27,7 @@ class BurgerBuilder extends Component {
         },
         totalPrice: 4,
         purchasable: false,
+        purchasing: false,
     }
 
     updatePurchaseState(ingredients) {
@@ -71,6 +72,14 @@ class BurgerBuilder extends Component {
         this.updatePurchaseState();
     }
 
+    purchaseHandler = () => {
+        this.setState({ purchasing: true });
+    }
+
+    purchaseCancelHandler = () => {
+        this.setState({ purchasing: false });
+    }
+
     render() {
         const disableInfo = {
             ...this.state.ingredients
@@ -80,7 +89,7 @@ class BurgerBuilder extends Component {
         }
         return (
             <Aux>
-                <Modal>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     <OrderSummary ingredients={this.state.ingredients} />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
@@ -90,6 +99,7 @@ class BurgerBuilder extends Component {
                     disabled={disableInfo}
                     purchasable={this.state.purchasable}
                     price={this.state.totalPrice}
+                    ordered={this.purchaseHandler}
                 />
             </Aux>
         );  
