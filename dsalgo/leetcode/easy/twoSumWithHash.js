b7fd4d7191e32d2b73b5d141e77de8cd7235c5c4
naracodes <naralee0124@gmail.com>
2021-03-09 14:46:30 -0500

    twoSumWithHash return indices

--- dsalgo/leetcode/easy/twoSumWithHash.js
+++ dsalgo/leetcode/easy/twoSumWithHash.js
@@ -1,5 +1,8 @@
+// Runtime: 76 ms, faster than 83.53% of JavaScript online submissions for Two Sum.
+// Memory Usage: 38.7 MB, less than 73.70% of JavaScript online submissions for Two Sum.
 
 
+// return nums
 const twoSum = (arr, target) => {
     const obj = {};
 
@@ -12,6 +15,22 @@ const twoSum = (arr, target) => {
     }
     return [];
 }
+
+// return indices of nums
+
+var twoSum = function(nums, target) {
+    const hash = {};
+    
+    for (let i = 0; i < nums.length; i++) {
+        const comp = target - nums[i];
+        const num = nums[i];
+        if (hash[comp] !== undefined) {
+            return [i, hash[comp]];
+        }
+        hash[num] = i;
+    }
+    return [];
+};
 
 console.log(twoSum([11,5,7,15,2], 9));
 // console.log(twoSum());
