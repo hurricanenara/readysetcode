6bc2027b399aece8f05786b37caa0953edc4501f
naracodes <naralee0124@gmail.com>
2021-03-11 12:02:23 -0500

    add axios/firebase functionality and add Spinner

--- react-1/burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ react-1/burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,8 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import Spinner from '../../components/UI/Spinner/Spinner';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -28,6 +30,7 @@ class BurgerBuilder extends Component {
         totalPrice: 4,
         purchasable: false,
         purchasing: false,
+        loading: false,
     }
 
     updatePurchaseState(ingredients) {
@@ -81,7 +84,29 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('Continuing!');
+        // alert('Continuing!');
+        this.setState({loading: true});
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice, // make sure to calculate price in the server side not client side (which might enable user to manipulate price)
+            customer: {
+                name: "Nara",
+                address: {
+                    street: 'test street',
+                    zipCode: '12345',
+                    country: 'US',
+                },
+                email: 'test@test.com'
+            },
+            deliveyMethod: 'fastest'
+        }
+        axios.post(`/orders.json`, order)
+            .then(res => {
+                this.setState({ loading:false, purchasing: false });
+            })
+            .catch(error => {
+                this.setState({ loading: false, purchasing: false });
+            });
     }
 
     render() {
@@ -91,16 +116,20 @@ class BurgerBuilder extends Component {
         for (let key in disableInfo) {
             disableInfo[key] = disableInfo[key] <= 0;
         }
+        let orderSummary = <OrderSummary
+            ingredients={this.state.ingredients}
+            price={this.state.totalPrice}
+            purchaseCancelled={this.purchaseCancelHandler}
+            purchaseContinued={this.purchaseContinueHandler}
+            />
+        if (this.state.loading) {
+            orderSummary = <Spinner />;
+        }
         return (
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     {/* {No need to rerender order summary when modal is hidden - performance check} */}
-                    <OrderSummary
-                     ingredients={this.state.ingredients}
-                     price={this.state.totalPrice}
-                     purchaseCancelled={this.purchaseCancelHandler}
-                     purchaseContinued={this.purchaseContinueHandler}
-                     />
+                    {orderSummary}
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls 
