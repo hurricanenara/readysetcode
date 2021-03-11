ad50519cfb67e1966cf249cbf2952f28a77537b0
naracodes <naralee0124@gmail.com>
2021-03-11 10:57:34 -0500

    add axios interceptors for request and response

--- react-1/posts/src/index.js
+++ react-1/posts/src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios'; //interceptors
+
+// interceptor/middleware
+let requestInterceptor = axios.interceptors.request.use(request => {
+    console.log(request);
+    return request; // you must return this otherwise you will block the request
+}, error => {
+    console.log(error);
+    return Promise.reject(error); // will fire if error with request like internet connectivity issue
+});
+
+axios.interceptors.request.eject(requestInterceptor);
+
+let responseInterceptor = axios.interceptors.response.use(response => {
+    console.log(response);
+    return response;
+}, error => {
+    console.log(error);
+    return Promise.reject(error); // will fire when request is made and error was received as response
+});
+axios.interceptors.response.eject(responseInterceptor);
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
