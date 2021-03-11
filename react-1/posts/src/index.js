e83bf399c5a3259eaae1c809a8ef3b4593c6bf9a
naracodes <naralee0124@gmail.com>
2021-03-11 11:07:03 -0500

    add axios instances

--- react-1/posts/src/index.js
+++ react-1/posts/src/index.js
@@ -5,8 +5,13 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios'; //interceptors
 
+axios.defaults.baseURL = `https://jsonplaceholder.typicode.com`; // half measure - instances if you have more than one base URL
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
 // interceptor/middleware
-let requestInterceptor = axios.interceptors.request.use(request => {
+// let requestInterceptor = 
+axios.interceptors.request.use(request => {
     console.log(request);
     return request; // you must return this otherwise you will block the request
 }, error => {
@@ -14,16 +19,17 @@ let requestInterceptor = axios.interceptors.request.use(request => {
     return Promise.reject(error); // will fire if error with request like internet connectivity issue
 });
 
-axios.interceptors.request.eject(requestInterceptor);
+// axios.interceptors.request.eject(requestInterceptor);
 
-let responseInterceptor = axios.interceptors.response.use(response => {
+// let responseInterceptor = 
+axios.interceptors.response.use(response => {
     console.log(response);
     return response;
 }, error => {
     console.log(error);
     return Promise.reject(error); // will fire when request is made and error was received as response
 });
-axios.interceptors.response.eject(responseInterceptor);
+// axios.interceptors.response.eject(responseInterceptor);
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
