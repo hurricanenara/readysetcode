9daa1bbf1d3a47f4da646b8d1c63dba0b8096ff8
naracodes <naralee0124@gmail.com>
2021-03-15 20:13:19 -0400

    add MaxHeap class

--- JavaScript/u-code/two/masterclass/heaps/MaxHeap.js
+++ JavaScript/u-code/two/masterclass/heaps/MaxHeap.js
@@ -20,11 +20,33 @@ class MaxBinaryHeap {
         }
         return this.values;
     }
+
+    extractMax() {
+        [this.values[0], this.values[this.values.length - 1]] = [this.values[this.values.length - 1], this.values[0]];
+        this.values.pop();
+        let currentValIdx = 0;
+        
+        let currentVal = this.values[currentValIdx];
+        let leftChildIdx = (2 * currentValIdx) + 1;
+        let rightChildIdx = (2 * currentValIdx) + 2;
+        
+        while (currentVal < this.values[leftChildIdx] && currentVal < this.values[rightChildIdx]) {
+            let leftChild = this.values[leftChildIdx];
+            let rightChild = this.values[rightChildIdx];
+            if (leftChild > rightChild || leftChild === rightChild) {
+                [this.values[currentValIdx], this.values[leftChildIdx]] = [this.values[leftChildIdx], this.values[currentValIdx]];
+                currentValIdx = leftChildIdx;
+            } else if (rightChild > leftChild) {
+                [this.values[currentValIdx], this.values[rightChildIdx]] = [this.values[rightChildIdx], this.values[currentValIdx]];
+                currentValIdx = rightChildIdx;
+            }
+        }
+        return this.values;
+    }
 }
 
 const heap = new MaxBinaryHeap();
 heap.values = [41, 39, 33, 18, 27, 12];
-console.log(heap.insert(55));
-console.log(heap.insert(1));
-console.log(heap.insert(45));
-console.log(heap.insert(99));
+console.log(heap.insert(55)); // [ 55, 39, 41, 18, 27, 12, 33 ]
+console.log(heap.extractMax());
+
