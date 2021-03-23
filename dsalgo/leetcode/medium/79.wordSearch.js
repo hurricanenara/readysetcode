291adfdb4653d26c65b6e0b550ca59ab16551d2f
naracodes <naralee0124@gmail.com>
2021-03-23 00:26:20 -0400

    solve multiple leetcode problems

--- dsalgo/leetcode/medium/79.wordSearch.js
+++ dsalgo/leetcode/medium/79.wordSearch.js
@@ -70,6 +70,45 @@ var exist = function(board, word) {
     return found;
 };
 
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function(board, word) {
+    
+    let found = false;
+    
+    function search(i, j, k) {
+        if (!found) {
+            if (i < 0 || j < 0 || i >= board.length || j >= board[0].length) return;
+            if (board[i][j] !== word[k]) return;
+            if (k === word.length - 1) {
+                found = true;
+                return;
+            }
+            
+            board[i][j] = null;
+            search(i + 1, j, k + 1);
+            search(i - 1, j, k + 1);
+            search(i, j + 1, k + 1);
+            search(i, j - 1, k + 1);
+            
+            board[i][j] = word[k];
+        }
+    }
+    
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[0].length; j++) {
+            if (board[i][j] === word[0]) {
+                search(i, j, 0);
+                if (found) return found;
+            }
+        }
+    }
+    return found;
+};
+
 console.log(exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCCED"));
 console.log(exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "SEE"));
 console.log(exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCB"));
