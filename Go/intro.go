6c330eaa8992683b3366280cda191ad7a42db3b1
naracodes <naralee0124@gmail.com>
2021-03-27 23:07:43 -0400

    add constant and variable

--- Go/intro.go
+++ Go/intro.go
@@ -8,4 +8,12 @@ import (
 func main() {
 	fmt.Println("Hello World")
 	fmt.Println(t.Now())
+
+	const earthsGravity = 9.80665
+	fmt.Println(earthsGravity)
+
+	// this will throw error
+	var jellybeanCounter int8
 }
+
+// useful commands go doc fmt.FUNCTION go doc time.Now etc
