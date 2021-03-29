5ed7eaf861e9e66755737416a16c4c6e67217bc3
naracodes <naralee0124@gmail.com>
2021-03-28 23:27:30 -0400

    default type

---
+++ Go/inferraing.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	daysOnVacation := 9
+	var hoursInDay = 24
+	var cupsOfCoffeeConsumed int
+	cupsOfCoffeeConsumed = 100
+	fmt.Println(cupsOfCoffeeConsumed)
+}
+
+// default in type
