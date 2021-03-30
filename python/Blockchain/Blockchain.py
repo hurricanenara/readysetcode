c12ea029daafb693caf3d1b0c23b6bd9552b9c4a
naracodes <naralee0124@gmail.com>
2021-03-29 23:23:49 -0400

    addblock method

--- python/Blockchain/Blockchain.py
+++ python/Blockchain/Blockchain.py
@@ -12,3 +12,16 @@ class Blockchain:
       transactions = []
       previous_hash = 0
       self.chain.append(Block(transactions, previous_hash))
+    # prints contents of blockchain
+    def print_blocks(self):
+        for i in range(len(self.chain)):
+            current_block = self.chain[i]
+            print("Block {} {}".format(i, current_block))
+            current_block.print_contents()    
+    
+    # add block to blockchain `chain`
+    def add_block(self, transactions):
+        previous_block = self.chain[len(self.chain) - 1].hash
+        new_block = Block(transactions, previous_block)
+        self.chain.append(new_block)
+
