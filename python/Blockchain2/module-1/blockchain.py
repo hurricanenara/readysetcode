f95699fb3d9b749a91895a96106a9fe9248d3829
naracodes <naralee0124@gmail.com>
2021-04-04 14:31:37 -0400

    finalize Blockchain class and methods

--- python/Blockchain2/module-1/blockchain.py
+++ python/Blockchain2/module-1/blockchain.py
@@ -23,5 +23,38 @@ class Blockchain:
     def get_previous_block(self):
         return self.chain[-1]
 
+    
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while not check_proof:
+            hash_operation = hashlib.sha256(str(new_proof ** 2 - previous_proof ** 2).encode()).hexdigest()
+            if hash_operation[:4] == '000':
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
+    
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys = True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+    
+    # is chain valid ? 1. previous_hash same as current block's previous_hash? 2. proof satisfies target?
+    def is_chain_valid(self, chain):
+        previous_block = chain[0]
+        block_index = 1
+        while block_index < len(chain):
+            block = chain[block_index]
+            if block['previous_has'] != self.hash(previous_block):
+                return False
+            previous_proof = previous_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(str(proof ** 2 - previous_proof ** 2).encode()).hexdigest()
+            if hash_operation[:4] != '0000':
+                return False
+            previous_block = block
+            block_index += 1
+        return True
+            
+    
 # Mining our Blockchain
-
