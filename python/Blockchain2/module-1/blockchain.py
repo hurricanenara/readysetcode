1158a5a9a8612eb7ae9579629e7a1042b72bbe5e
naracodes <naralee0124@gmail.com>
2021-04-04 17:16:46 -0400

    add is_valid endpoint

--- python/Blockchain2/module-1/blockchain.py
+++ python/Blockchain2/module-1/blockchain.py
@@ -1,17 +1,23 @@
-# Module 1 - create a blockchain
+# Module 1 - Create a Blockchain
 
+# To be installed:
+# Flask==0.12.2: pip install Flask==0.12.2
+# Postman HTTP Client: https://www.getpostman.com/
+
+# Importing the libraries
 import datetime
 import hashlib
 import json
 from flask import Flask, jsonify
 
-# Buliding a Blockchain
+# Part 1 - Building a Blockchain
 
 class Blockchain:
-    
+
     def __init__(self):
         self.chain = []
         self.create_block(proof = 1, previous_hash = '0')
+
     def create_block(self, proof, previous_hash):
         block = {'index': len(self.chain) + 1,
                  'timestamp': str(datetime.datetime.now()),
@@ -23,13 +29,12 @@ class Blockchain:
     def get_previous_block(self):
         return self.chain[-1]
 
-    
     def proof_of_work(self, previous_proof):
         new_proof = 1
         check_proof = False
-        while not check_proof:
-            hash_operation = hashlib.sha256(str(new_proof ** 2 - previous_proof ** 2).encode()).hexdigest()
-            if hash_operation[:4] == '000':
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
                 check_proof = True
             else:
                 new_proof += 1
@@ -39,22 +44,87 @@ class Blockchain:
         encoded_block = json.dumps(block, sort_keys = True).encode()
         return hashlib.sha256(encoded_block).hexdigest()
     
-    # is chain valid ? 1. previous_hash same as current block's previous_hash? 2. proof satisfies target?
     def is_chain_valid(self, chain):
         previous_block = chain[0]
         block_index = 1
         while block_index < len(chain):
             block = chain[block_index]
-            if block['previous_has'] != self.hash(previous_block):
+            if block['previous_hash'] != self.hash(previous_block):
                 return False
             previous_proof = previous_block['proof']
             proof = block['proof']
-            hash_operation = hashlib.sha256(str(proof ** 2 - previous_proof ** 2).encode()).hexdigest()
+            hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
             if hash_operation[:4] != '0000':
                 return False
             previous_block = block
             block_index += 1
         return True
-            
-    
-# Mining our Blockchain
+
+# Part 2 - Mining our Blockchain
+
+# Creating a Web App
+app = Flask(__name__)
+
+# Creating a Blockchain
+blockchain = Blockchain()
+
+@app.route('/')
+def index():
+    return 'home'
+
+# Mining a new block
+@app.route('/mine_block', methods = ['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message': 'Congratulations, you just mined a block!',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']}
+    return jsonify(response), 200
+
+# Getting the full Blockchain
+@app.route('/get_chain', methods = ['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)}
+    return jsonify(response), 200
+
+# Checking if the Blockchain is valid
+@app.route('/is_valid', methods = ['GET'])
+def is_valid():
+    if blockchain.is_chain_valid(blockchain.chain):
+        return jsonify({ 'message': 'chain is valid' })
+    else:
+        return jsonify({ 'message': 'chain is not valid' })
+
+# Running the app
+app.run(host = '0.0.0.0', port = 5000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
