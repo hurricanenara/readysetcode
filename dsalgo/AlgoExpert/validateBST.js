45d79dcf1287b8b4f90af6df915e5f2b8ec19309
naracodes <naralee0124@gmail.com>
2021-04-10 11:55:03 -0400

    add a bst to test step by step

--- dsalgo/AlgoExpert/validateBST.js
+++ dsalgo/AlgoExpert/validateBST.js
@@ -1,4 +1,4 @@
-class BST {
+class Node {
   constructor(value) {
     this.value = value;
     this.left = null;
@@ -19,3 +19,24 @@ function validateBSTHelper(node, minValue, maxValue) {
 
   return leftIsValid && rightIsValid;
 }
+
+let node10 = new Node(10);
+let node2 = new Node(2);
+let node5 = new Node(5);
+let node15 = new Node(15);
+let node13 = new Node(13);
+let node22 = new Node(22);
+let node14 = new Node(14);
+let node1 = new Node(1);
+let node5ii = new Node(5);
+
+node10.left = node5;
+node5.left = node2;
+node2.left = node1;
+node5.right = node5ii;
+node10.right = node15;
+node15.left = node13;
+node15.right = node22;
+node13.right = node14;
+
+console.log(validateBST(node10));
