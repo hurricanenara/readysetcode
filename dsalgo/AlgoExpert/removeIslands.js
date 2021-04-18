b5b47e5ad189410d4248901dc1be9f3d89e2bdd8
naracodes <naralee0124@gmail.com>
2021-04-17 22:15:22 -0400

    set up row and col checks for removeIslands

---
+++ dsalgo/AlgoExpert/removeIslands.js
@@ -0,0 +1,21 @@
+function removeIslands(matrix) {
+  // check top row
+  for (let c = 0; c < matrix[0].length; c++) {
+    matrix[0][c];
+  }
+
+  //check left col
+  for (let r = 0; r < matrix.length; r++) {
+    matrix[r][1];
+  }
+
+  //check right col
+  for (let r = 0; r < matrix.length; r++) {
+    matrix[r][matrix[0].length];
+  }
+
+  // check bottom row
+  for (let c = 0; c < matrix[0].length; c++) {
+    matrix[matrix.length][c];
+  }
+}
