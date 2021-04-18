4525e8e01d59ed5c17d48584c2605f2ee3665921
naracodes <naralee0124@gmail.com>
2021-04-18 15:35:33 -0400

    set up graphiql middleware

--- graphql/lesson_1/users/server.js
+++ graphql/lesson_1/users/server.js
@@ -1,8 +1,16 @@
 import express from "express";
+import expressGraphQL from "express-graphql";
 
 const app = express();
 
 const PORT = 4000;
+
+app.use(
+  "/graphql",
+  expressGraphQL({
+    graphiql: true,
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}`);
