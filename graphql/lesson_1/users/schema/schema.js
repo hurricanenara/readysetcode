b1dc6d1ffb97fb76267548b31509b970ded0a088
naracodes <naralee0124@gmail.com>
2021-04-18 22:19:57 -0400

    connect graphiql to schema and server

--- graphql/lesson_1/users/schema/schema.js
+++ graphql/lesson_1/users/schema/schema.js
@@ -1,6 +1,19 @@
 const graphql = require("graphql");
+const _ = require("lodash");
 
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull,
+} = graphql;
+
+const users = [
+  { id: "20", firstName: "Paul", age: 20 },
+  { id: "24", firstName: "Nara", age: 20 },
+];
 
 const UserType = new GraphQLObjectType({
   name: "User",
@@ -9,4 +22,21 @@ const UserType = new GraphQLObjectType({
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
   },
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
--- graphql/lesson_1/users/server.js
+++ graphql/lesson_1/users/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
 
 const app = express();
 
@@ -8,6 +9,7 @@ const PORT = 4000;
 app.use(
   "/graphql",
   graphqlHTTP({
+    schema,
     graphiql: true,
   })
 );
